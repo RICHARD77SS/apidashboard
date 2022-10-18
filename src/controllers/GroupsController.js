063ff88f36a5503721f88fc9067672264ad0817f
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-18 17:00:58 -0300

    Conteoller de grupos

---
+++ src/controllers/GroupsController.js
@@ -0,0 +1,122 @@
+const { v4: uuid } = required('uuid');
+const Groups = require('../models/groups')
+
+module.exports = {
+  async index(req, res) {
+    try {
+      const group = await Groups.find();
+      return res.status(200).json({ groups })
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  },
+  async store(req, res) {
+    const {
+      name,
+      creationDate,
+      weekDay,
+      sex,
+      time,
+      category,
+      originGroup,
+      lider1,
+      lider2,
+      lider3,
+      lider4,
+      address,
+      district,
+      number,
+      country,
+      state,
+      city,
+      anotations
+    } = req.body;
+    if (!name) {
+      return res.status(400).json({ error: 'Missing' })
+    }
+    const groups = new Groups({
+      _id: uuid(),
+      name,
+      creationDate,
+      weekDay,
+      sex,
+      time,
+      category,
+      originGroup,
+      lider1,
+      lider2,
+      lider3,
+      lider4,
+      address,
+      district,
+      number,
+      country,
+      state,
+      city,
+      anotations
+    })
+    try {
+      await positions.save();
+      return res.status(201).json({ message: 'Groups added successfully!' })
+    } catch (error) {
+      res.status(400).json({ error: error.message })
+    }
+  },
+  async update(req, res) {
+    const {
+      name,
+      creationDate,
+      weekDay,
+      sex,
+      time,
+      category,
+      originGroup,
+      lider1,
+      lider2,
+      lider3,
+      lider4,
+      address,
+      district,
+      number,
+      country,
+      state,
+      city,
+      anotations
+    } = req.body;
+    if (!name && !creationDate && !weekDay && !sex && !time && !category && !originGroup && !lider1 && !lider2 && !lider3 && !lider4 && !address && !district && !number && !country && !state && !city && !anotations) {
+      return res.status(200).json({ error: 'You must inform a new name' })
+    }
+    if (name) res.groups.name = name
+    if (creationDate) res.groups.creationDate = creationDate
+    if (weekDay) res.groups.weekDay = weekDay
+    if (sex) res.groups.sex = sex
+    if (time) res.groups.time = time
+    if (category) res.groups.category = category
+    if (originGroup) res.groups.originGroup = originGroup
+    if (lider1) res.groups.lider1 = lider1
+    if (lider2) res.groups.lider2 = lider2
+    if (lider3) res.groups.lider3 = lider3
+    if (lider4) res.groups.lider4 = lider4
+    if (address) res.groups.address = address
+    if (district) res.groups.district = district
+    if (number) res.groups.number = number
+    if (country) res.groups.country = country
+    if (state) res.groups.state = state
+    if (city) res.groups.city = city
+    if (anotations) res.groups.anotations = anotations
+    try {
+      await res.group.save();
+      return res.status(200).json({ message: 'updated successfully' })
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  },
+  async delete(req, res) {
+    try {
+      await res.positions.remove()
+      return res.status(200).json({message: 'deted successfully'})
+    } catch (error) {
+      return res.status(500).json({error: error.message})
+    }
+  },
+}
