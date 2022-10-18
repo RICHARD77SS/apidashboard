52dca235b2ac618637b5608cb5f58f3f658be31d
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-18 17:53:55 -0300

    modelos de grupos

---
+++ src/models/groups.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose')
+
+const groupSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  creationDate: {
+    type: Date,
+    required: false,
+  },
+  weekDay: {
+    type: String,
+    required: false,
+  },
+  sex: {
+    type: String,
+    required: false,
+  },
+  time: {
+    type: String,
+    required: false,
+  },
+  category: {
+    type: String,
+    required: false,
+  },
+  originGroup: {
+    type: String,
+    required: false,
+  },
+  lider1: {
+    type: String,
+    required: false,
+  },
+  ldier2: {
+    type: String,
+    required: false,
+  },
+  lider3: {
+    type: String,
+    required: false,
+  },
+  lider4: {
+    type: String,
+    required: false,
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+  district: {
+    type: String,
+    required: false,
+  },
+  number: {
+    type: Number,
+    required: false,
+  },
+  country: {
+    type: String,
+    required: false,
+  },
+  state: {
+    type: String,
+    required: false,
+  },
+  city: {
+    type: String,
+    required: false,
+  },
+  anotations: {
+    type: String,
+    required: false,
+  },
+})
