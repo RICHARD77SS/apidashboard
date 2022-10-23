cbd2f533f40f5312f5811b9308879eb746ae14fc
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-23 20:23:38 -0300

    Modelo financias

---
+++ src/models/financial.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const financialSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  value: {
+    type: Number,
+    required: false,
+  },
+  paidOut: {
+    type: String,
+    required: false,
+  },
+  receivedFrom: {
+    type: String,
+    required: false,
+  },
+  category: {
+    type: String,
+    required: false,
+  },
+  account: {
+    type: String,
+    required: false,
+  },
+  costCenter: {
+    type: String,
+    required: false,
+  },
+  typeOfPayment: {
+    type: String,
+    required: false,
+  },
+  documentNumber: {
+    type: String,
+    required: false,
+  },
+  notes: {
+    type: String,
+    required: false,
+  },
+  competence: {
+    type: Date,
+    required: false,
+  },
+  file: {
+    type: String,
+    required: false,
+  }
+})
+module.exports = mongoose.model('Financial', financialSchema);
