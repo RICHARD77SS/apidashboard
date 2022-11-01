c21734cadcfd3b8f98875c0e2fa8c9c5c3366e59
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-11-01 20:29:57 -0300

    Modelo para estudos

---
+++ src/models/studies.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const studiesSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: false,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  file: {
+    type: String,
+    required: false,
+  },
+  notification: {
+    type: Boolean,
+    required: false,
+  }
+})
+
+module.exports = mongoose.model('Studies', studiesSchema);
