a4f69f216ab08e8ae9250092b7ad62c6257551fc
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-11-04 20:49:40 -0300

    Modelo para reuniões

---
+++ src/models/meetings.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const meetingsSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  group: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: Number,
+    required: false,
+  },
+  participants: {
+    type: [],
+    required: false,
+  },
+  visitors: {
+    type: [],
+    required: false,
+  },
+  notes: {
+    type: String,
+    required: false,
+  },
+})
+
+module.exports = mongoose.model('meetings', meetingsSchema);
