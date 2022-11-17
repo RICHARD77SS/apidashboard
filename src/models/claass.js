70db6f740335d969d418529065baac35403b2743
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-11-17 20:55:38 -0300

    Model para turma

---
+++ src/models/claass.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const claassSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  school: {
+    type: String,
+    required: false,
+  },
+  time: {
+    type: String,
+    required: false,
+  },
+  status: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  date: {
+    type: Date,
+    required: false,
+  }
+})
+
+module.exports = mongoose.model('Claass', claassSchema)
