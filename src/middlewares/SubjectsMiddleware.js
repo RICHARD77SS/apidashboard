3f04e23427d1e0d69a2b4f41ded145904ad647fb
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-11-19 20:15:23 -0300

    Middleware para disciplinas

---
+++ src/middlewares/SubjectsMiddleware.js
@@ -0,0 +1,21 @@
+const { validate: isUuid } = require('uuid');
+const Subjects = require('../models/subjects')
+
+module.exports = {
+  async validateId(req, res, next) {
+    const { id } = req.params;
+    if (!isUuid(id)) {
+      return res.status(400).json({ error: 'Invalid Id' })
+    }
+    try {
+      const subjects = await Subjects.findById(id);
+      res.subjects = subjects;
+      if (!subjects) {
+        return res.status(404).json({ error: 'subjects not found' })
+      }
+    } catch (error) {
+      return res.status(500).json({ error: error.message })
+    }
+    next()
+  }
+}
