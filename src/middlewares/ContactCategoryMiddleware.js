33fa0276c0560fe7c9b12c4d1920e0f4ed85c6fa
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-01 22:45:53 -0300

    Midleware categoria de contatos

---
+++ src/middlewares/ContactCategoryMiddleware.js
@@ -0,0 +1,21 @@
+const { validate: isUuid } = require('uuid')
+const ContactCategory = require('../models/contactCategory');
+
+module.exports = {
+  async validateId(req, res, next) {
+    const { id } = req.params;
+    if (!isUuid(id)) {
+      return res.status(400).json({ error: 'invalid Id' });
+    }
+    try {
+      const contactCategory = await ContactCategory.findById(id);
+      res.contactCategory = contactCategory;
+      if (!contactCategory) {
+        return res.status(404).json({ error: 'ContactCategory not found' })
+      }
+    } catch (error) {
+      return res.status(500).json({ error: error.message })
+    }
+    next()
+  }
+}
