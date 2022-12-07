08ae607b1c8c3608b76f5192a7beb4e61fbe575f
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-06 22:39:25 -0300

    Model contatos

---
+++ src/models/contactCategory.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const contactCategorySchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  }
+})
+
+module.exports = mongoose.model('ContactCategory', contactCategorySchema);
