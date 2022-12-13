226b289d82753b487ce79b833518dd523f01140d
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-12 22:07:31 -0300

    Controller categoria de patrimonios

---
+++ src/controllers/CategoryPatrimoniesController.js
@@ -0,0 +1,62 @@
+const { v4: uuid } = require('uuid');
+const CategoryPatrimonies = require('../models/categoryPatrimonies');
+
+module.exports = {
+  async index(req, res) {
+    try {
+      const categoryPatrimonies = await CategoryPatrimonies.find();
+      return res.status(200).json({ categoryPatrimonies })
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  },
+  async store(req, res) {
+    const {
+      name,
+      description
+    } = req.body;
+    if (!name) {
+      return res.status(400).json({
+        error: 'Missing name '
+      })
+    }
+    const categoryPatrimonies = new CategoryPatrimonies({
+      _id: uuid(),
+      name,
+      description
+    })
+    try {
+      await categoryPatrimonies.save();
+      return res.status(201).json({ message: 'CategoryPatrimonies added successfully!' })
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  },
+  async update(req, res) {
+    const {
+      name,
+      description
+    } = req.body;
+    if (!name && !description) {
+      return res.status(200).json({
+        error: 'you must inform a new name or new description'
+      })
+    }
+    if (name) res.categoryPatrimonies.name = name;
+    if (description) res.categoryPatrimonies.description = description;
+    try {
+      await res.categoryPatrimonies.save();
+      return res.status(200).json({ message: 'CategoryPatrimonies Updated successfully' })
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  },
+  async delete(req, res) {
+    try {
+      await res.categoryPatrimonies.remove();
+      return res.status(200).json({ message: 'deleted successfully' })
+    } catch (error) {
+      return res.status(500).json({ error: error.message })
+    }
+  },
+}
