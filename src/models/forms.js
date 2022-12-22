1d0936c1df469773a5805c89513604ade730ee9f
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-21 21:35:53 -0300

    Model para forms

---
+++ src/models/forms.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const formSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: false,
+  },
+  caption: {
+    type: String,
+    required: false,
+  },
+  status: {
+    type: String,
+    required: false,
+  },
+  notification: {
+    type: Boolean,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  additionalFields: {
+    type: [],
+    required: false,
+  }
+})
+module.exports = mongoose.model('Form', formSchema)
