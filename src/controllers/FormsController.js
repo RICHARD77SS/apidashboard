c664e72c5afc1b91558fc603fcf1cacf4f4bab1c
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-21 21:36:57 -0300

    Controller para forms

---
+++ src/controllers/FormsController.js
@@ -0,0 +1,83 @@
+const { v4: uuid } = require('uuid')
+const Forms = require('../models/forms')
+
+module.exports = {
+  async index(req, res) {
+    try {
+      const forms = await Forms.find()
+      return res.status(200).json({ forms })
+    } catch (error) {
+      return res.status(500).json({ error: error.message })
+    }
+  },
+  async store(req, res) {
+    const {
+      title,
+      caption,
+      status,
+      notification,
+      description,
+      additionalFields
+    } = req.body;
+    if (!title) {
+      return res.status(400).json({
+        error: 'Missing title'
+      })
+    }
+    const forms = new Forms({
+      _id: uuid(),
+      title,
+      caption,
+      status,
+      notification,
+      description,
+      additionalFields
+    })
+    try {
+      await forms.save()
+      return res.status(201).json({ message: 'Form Added successfully!' })
+    } catch (error) {
+      res, status(400).json({ error: error.message })
+    }
+  },
+  async update(req, res) {
+    const {
+      title,
+      caption,
+      status,
+      notification,
+      descriprion,
+      additionalFields
+    } = req.body;
+    if (!title
+      && !caption
+      && !status
+      && !notification
+      && !descriprion
+      && !additionalFields) {
+      return res.status(200).json({
+        error: 'You must inform a new title or new info'
+      })
+    }
+    if (title) res.forms.title = title;
+    if (caption) res.forms.caption = caption;
+    if (status) res.forms.status = status;
+    if (notification) res.forms.notification = notification;
+    if (descriprion) res.forms.descriprion = descriprion;
+    if (additionalFields) res.forms.additionalFields = additionalFields;
+    try {
+      await res.forms.save();
+      return res.status(200).json({ message: 'form updated successfully!' })
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  },
+  async delete(req, res) {
+    try {
+      await res.forms.remove();
+      return res.status(200).json({ message: 'deleted successfully!' })
+    } catch (error) {
+      return res.status(500).json({ error: error.message })
+    }
+  },
+}
