7135a7cc3d75be3b17527de4d0c8ee9c4a5e3c17
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-22 22:06:40 -0300

    Model para documentos

---
+++ src/models/documents.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const documentsSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  }
+})
+
+module.exports = mongoose.model('Documents', documentsSchema);
